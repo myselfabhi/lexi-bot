9cce3ee124b63c7fdec0279f6509d19b92e0eba9
myselfabhi <abhinavverma2024@gmail.com>
2025-02-15 16:30:28 +0530

    feat: node, express setup with typescript

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 const app = express();
+//middleware
+app.use(express.json());
+//connections and listeners
 app.listen(5000, () => {
     console.log('Server is running on http://localhost:5000');
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
+
+
+//connections and listeners
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
 })
